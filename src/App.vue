e1e8eeb708e6b770dffc44258d09ab2ed2b094c1
wwml725 <wwml725@163.com>
2019-07-03 18:45:24 +0800

    0001.搭建开发环境

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+   hello word
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus">
+@import "common/stylus/variable.styl";
+  #app
+    color: $color-theme
+</style>
