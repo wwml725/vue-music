283bd081b0c3e67ba7c3c49051a5cea705e55ea5
wwml725 <wwml725@163.com>
2019-07-17 17:49:29 +0800

    0015.播放器页面的vuex数据设计

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,20 @@
 export const singer = state => state.singer;
+
+export const playing = state => state.playing;
+
+export const fullScreen = state => state.fullScreen;
+
+export const playList = state => state.playList;
+
+export const sequenceList = state => state.sequenceList;
+
+export const mode = state => state.mode;
+
+export const currentIndex = state => state.currentIndex;
+
+//getter除了起一个代理的作用，还可以作为一个计算属性来使用
+//当前歌曲
+export const currentSong = (state)=>{
+  return tate.palyList[state.currentIndex]||{}
+};
+
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,15 @@
+import {playMode} from 'common/js/config'
 const state = {
-  singer: {},
+  singer: {},//歌手信息
+  //播放器所需数据
+  playing:false,//是否正在播放
+  fullScreen:false,//是否显示全屏面板（或者小面板）
+  playList:[],//播放列表
+  sequenceList:[],//随机播放列表相关
+  //顺序播放的时候playList和sequenceList列表是一样的
+  mode:playMode.sequence,//播放模式  是顺序还是随机
+  currentIndex:-1,//当前播放的歌曲索引
+  //如果想播放下一首，只需要控制currentIndex
 }
 
 export default state
