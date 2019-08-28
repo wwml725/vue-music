61aee43bbefc02da44668a282dd4451bbc8a0f0e
wwml725 <wwml725@163.com>
2019-08-28 23:01:21 +0800

    0050.创建个人中心页面，添加返回按钮，和switches组件的引入

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Singer from 'components/singer/singer'
 import SingerDetail from 'components/singer-detail/singer-detail'
 import Disc from 'components/disc/disc'
 import TopList from 'components/top-list/top-list'
+import UserCenter from 'components/user-center/user-center'
 
 Vue.use(Router)
 
@@ -64,5 +65,9 @@ export default new Router({
         }
       ]
     },
+    {
+      path: '/user',
+      component: UserCenter
+    }
   ]
 })
